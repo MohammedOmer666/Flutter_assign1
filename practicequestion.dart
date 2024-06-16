cffd8ab5b19f563aacf59d4e027647d97ce7e4b4
MohammedOmer666 <102437601+MohammedOmer666@users.noreply.github.com>
2024-06-16 13:38:11 -0700

    Add files via upload

---
+++ practicequestion.dart
@@ -0,0 +1,124 @@
+/*void main() {
+  int length = 10;
+  int breath = 5;
+  if (length == breath) {
+    print("Its Square");
+  } else {
+    print("Its rectangle");
+  }
+}
+
+void main() {
+  int age1 = 25;
+  int age2 = 40;
+
+  if (age1 > age2) {
+    print("First one is older than second one");
+  } else {
+    print("Second person is older than first person");
+  }
+}
+*/
+/*void main() {
+  num attendence = 50;
+  num classattend = 35;
+  num percentage = (classattend / attendence) * 100;
+
+  if (percentage == 75) {
+    print("student allowed");
+  } else {
+    print("not allowed");
+  }
+}*/
+/*void main() {
+  double celcius = 25;
+  double farhenhiet = (celcius * 9 / 5) + 32;
+  print("farhenhiet = $farhenhiet");
+}*/
+
+/*void main() {
+  num temp = 30;
+  if (temp < 0) {
+    print("freezing weather");
+  } else if (temp < 10) {
+    print("very cold weather");
+  } else if (temp >= 10 && temp <= 20) {
+    print("cold weather");
+  } else if (temp >= 20 && temp <= 30) {
+    print("normal");
+  } else if (temp >= 30 && temp <= 40) {
+    print("its hot");
+  } else if (temp >= 40) {
+    print("its very hot");
+  }
+}*/
+
+/*void main() {
+  String vowel = 'z';
+  if (vowel == 'A' ||
+      vowel == 'E' ||
+      vowel == 'E' ||
+      vowel == 'I' ||
+      vowel == 'O' ||
+      vowel == 'U') {
+    print("its a vowel");
+  } else {
+    print("not a vowel");
+  }
+}
+
+import 'dart:math';
+
+
+
+void main() {
+  string name;
+  int rollname;
+  string class;
+  double percentage;
+  string grade;
+
+  
+}
+
+
+
+void main() {
+  List list1 = [1, 2, 3, 4, 5, 6, "abc"];
+  print(list1.length);
+  print(list1.reversed.runtimeType);
+  print(list1.isEmpty);
+  print(list1.reversed.toList());
+  print(list1.isNotEmpty);
+  list1.add("New value");
+  list1.insert(2, 'aafan');
+  print(list1);
+  print("New list $list1");
+  list1.remove(5);
+  print(list1);
+  list1.removeAt(1);
+  print(list1);
+  list1.insert(2, [96]);
+  print(list1);
+}
+*/
+// void main() {
+//   List L1 = [
+//     'monday',
+//     'tuesday',
+//     'wednesday',
+//     'Thursday',
+//     'friday',
+//     'saturday',
+//     'sunday'
+//   ];
+//   L1.removeAt([0] as int, [1] as int, [2], [3], [4], [5], [6]);
+//   print(L1);
+// }
+
+void main() {
+  List L1 = [];
+  print(L1);
+  L1.addAll(['lemon', 'mango', 'apple', 'orange', 'melon']);
+  print(L1);
+}
